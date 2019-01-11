724b6eb15b4d2e7b54b3b713fdbf3a4a6f8959a1
Akira Shinohara <k017c1067@it-neec.jp>
2019-01-11 15:31:46 +0000

    [new] IndexViewを定義

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import User
 
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    model = User
