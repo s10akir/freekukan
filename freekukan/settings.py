d79ad9c401bda02967147935c039998d95125d4d
Akira Shinohara <k017c1067@it-neec.jp>
2019-01-11 14:06:23 +0000

    [change] localhost外からの接続を許可

--- freekukan/settings.py
+++ freekukan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-hk2z6tp9n0^zipa*kha5xiwybtzh#l0$2$g7i4v2(7l944bag'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
